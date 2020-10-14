dbfe98915da8a92d36b2342f95e84081c1e80312
Rmx <ramiro-suarez-95@hotmail.com>
2020-10-14 13:58:30 -0500

    Se creo las interfaces de login y sugnup

--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -6,4 +6,24 @@ router.get('/', function (req, res) {
   res.render('index', {
   })
 })
+
+router.get('/login', function (req, res) {
+  res.render('login', {
+  })
+})
+
+router.get('/signup', function (req, res) {
+  res.render('signup', {
+  })
+})
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////77
 module.exports = router
