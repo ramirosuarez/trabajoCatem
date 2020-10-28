eae4bdb97b2f9fc678dd073eed9c9cff523b4ad2
Rmx <ramiro-suarez-95@hotmail.com>
2020-10-28 11:54:56 -0600

    se configuro para heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 var path = require('path')
 //Apunta a otro archivo que sirve como router principal
